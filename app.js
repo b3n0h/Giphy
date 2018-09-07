703a2aa1044e07ba1a113bc2069ab1e370a5f05b
benohhh <b3njaminoh@gmail.com>
2018-09-06 17:36:22 -0700

    created temp function to display gifs

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-let giphyApi = 'https://api.giphy.com/v1/gifs/trending/api_key=7OKg0xxG9vaCmcx7Vh3f9zE1PEkIgjBG'
+// displays gifs
+// function displayGif () {
+  let gif = $(this).attr('data-gif')
+  let tempgif = 'cat'
+  let searchUrl = 'https://api.giphy.com/v1/gifs/search?q=' + tempgif + '&api_key=7OKg0xxG9vaCmcx7Vh3f9zE1PEkIgjBG&limit=10'
+  $.ajax({
+    url: searchUrl,
+    method: "GET"
+  }).then(function (r) {
+    r.data.forEach(function (data) {
+      $('#gifs').append(`
+        <div>
+          <img src='${data.images.original.url}' alt='stillgif'>
+        </div>
+      `)
+    })
+    console.log(r);
+  })
+// }
 
-fetch(giphyApi)
-.then(function (r) {
-  return r.json
-})
-.then(function (data) {
-  console.log(data)
-})
-.catch(function (e) {
-  console.log(e)
-})
+// adds new buttons
+// function newButtons () {
+//   event.preventDefault()
+//   let gifButton = $('#searchGif')
+// }
+
+// // handles search gif button click
+// $('#findGif').on('click', function () {
+
+// })
