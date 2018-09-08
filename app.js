c704a7ea1ddfcad9f3e15b9297597a710ba3f70f
benohhh <b3njaminoh@gmail.com>
2018-09-07 23:56:52 -0700

    gave generated buttons attr of gif

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // displays gifs
-// function displayGif () {
-  let gif = $(this).attr('data-gif')
-  let tempgif = 'cat'
-  let searchUrl = 'https://api.giphy.com/v1/gifs/search?q=' + tempgif + '&api_key=7OKg0xxG9vaCmcx7Vh3f9zE1PEkIgjBG&limit=10'
+function displayGif (gif) {
+  let searchUrl = 'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=7OKg0xxG9vaCmcx7Vh3f9zE1PEkIgjBG&limit=10'
   $.ajax({
     url: searchUrl,
     method: "GET"
@@ -10,21 +8,35 @@
     r.data.forEach(function (data) {
       $('#gifs').append(`
         <div>
-          <img src='${data.images.original.url}' alt='stillgif'>
+          <img src='${data.images.original.url}' alt='gif'>
         </div>
       `)
     })
+    $('#searchGif').val('')
     console.log(r);
   })
-// }
+}
 
-// adds new buttons
-// function newButtons () {
-//   event.preventDefault()
-//   let gifButton = $('#searchGif')
-// }
+function newButtons (gif) {
+  $('#gifButtons').append(`
+    <button type="button" class="btn btn-secondary btn-sm" id="newGifBtn">${gif}</button>
+  `)
+  $('#gifButtons').attr(gif)
+}
 
-// // handles search gif button click
-// $('#findGif').on('click', function () {
+// create empty function
+function empty () {
+}
 
-// })
+// handles search gif button click
+$('#findGif').on('click', function () {
+  event.preventDefault()
+  let gif = $('#searchGif').val()
+  displayGif(gif)
+  newButtons(gif)
+})
+
+// handles new gif button click
+$('#newGifBtn').on('click', function () {
+  
+})
