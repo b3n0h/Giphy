c8dadc39f41efc9361f89b23ccd63b863a2e7476
benohhh <b3njaminoh@gmail.com>
2018-09-09 20:54:35 -0700

    grabbed ratings and added bg color

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let gif
+
 // displays gifs
 function displayGif (gif) {
   let searchUrl = 'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=7OKg0xxG9vaCmcx7Vh3f9zE1PEkIgjBG&limit=10'
@@ -8,8 +10,10 @@ function displayGif (gif) {
     r.data.forEach(function (data) {
       $('#gifs').append(`
         <div>
-          <img src='${data.images.original.url}' alt='gif'>
+          <img src='${data.images.fixed_height_still.url}' data-still="${data.images.fixed_height_still.url}" data-gif="${data.images.fixed_height.url}" class="giphy" alt="gif">
         </div>
+        <p>Rating: ${data.rating.toUpperCase()}</p>
+        <hr>
       `)
     })
     $('#searchGif').val('')
@@ -17,26 +21,46 @@ function displayGif (gif) {
   })
 }
 
-function newButtons (gif) {
-  $('#gifButtons').append(`
-    <button type="button" class="btn btn-secondary btn-sm" id="newGifBtn">${gif}</button>
-  `)
-  $('#gifButtons').attr(gif)
-}
+// inital array
+let IniBtns = ['tennis', 'basketball', 'volleyball', 'hockey', 'football']
 
-// create empty function
-function empty () {
+// shows initial buttons
+function showButtons () {
+  $('#gifButtons').empty()
+  for (i = 0; i < IniBtns.length; i++) {
+    $('#gifButtons').append(`<button type="button" class="btn btn-secondary btn-sm btnArr mr-2">${IniBtns[i]}</button>`)
+  }
+}
+showButtons()
+
+// handles when new gif button is created
+function newButtons (gif) {
+  IniBtns.push(gif)
+  $('#gifButtons').attr(gif)
+  showButtons()
 }
 
 // handles search gif button click
 $('#findGif').on('click', function () {
   event.preventDefault()
-  let gif = $('#searchGif').val()
-  displayGif(gif)
+  gif = $('#searchGif').val()
   newButtons(gif)
 })
 
 // handles new gif button click
-$('#newGifBtn').on('click', function () {
-  
+$(document).on('click', '.btnArr', function () {
+  gif = $(this).text()
+  displayGif(gif)
+})
+
+// handles playing gifs
+$(document).on('click', '.giphy', function () {
+  let imgSrc = $(this).attr("src")
+  let still = $(this).attr("data-still")
+  let giphy = $(this).attr("data-gif")
+  if (imgSrc === still) {
+    $(this).attr("src", giphy)
+  } else {
+    $(this).attr("src", still)
+  }
 })
